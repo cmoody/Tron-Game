4ccfa14fa809aeb9d52207a5e0c61bfd3cf452fa
Chase Moody <chase.moody@mutualmobile.com>
2014-06-11 13:48:18 -0500

    added bower and started splitting into modules

---
+++ src/js/app/drawCycle.js
@@ -0,0 +1,33 @@
+define(function(require) {
+	"use strict";
+
+	var Game = require('app/game');
+	// Needs access to player1 and enemy obj
+
+	function drawCycle() {
+		Game.ctx.save();
+
+		Game.ctx.shadowBlur = 10;
+		Game.ctx.shadowColor = 'rgba(255, 255, 255, 0.4)';
+
+		// Draws Player1 Cycle
+		// Rotate when going left/right
+		//ctx.drawImage(tronCycle, posx, posy);
+		Game.ctx.beginPath();
+		Game.ctx.rect(player1.coordinates[currentRound].posx, player1.coordinates[currentRound].posy, 4, 4);
+		Game.ctx.fillStyle = player1.color;
+		Game.ctx.fill();
+
+		// Draws Enemy Cycle
+		//ctx.drawImage(tronCycle, posx, posy);
+		Game.ctx.beginPath();
+		Game.ctx.rect(enemy.coordinates[currentRound].posx, enemy.coordinates[currentRound].posy, 4, 4);
+		Game.ctx.fillStyle = enemy.color;
+		Game.ctx.fill();
+
+		Game.ctx.restore();
+	}
+
+	return drawCycle;
+
+});
---
+++ src/js/app/enemyAI.js
@@ -0,0 +1,90 @@
+define(function(require) {
+	"use strict";
+
+	// Needs access to enemy object
+
+	function enemyAI() {
+		var posx, posy;
+		// Get current position
+		// Add +1 to current positions direction
+		// If +1 is == to player1 coordinates or enemy coordinates randomly choose left or right
+		// check if left or right is in arrays and if so go other way
+
+		// Insert pick direction function
+		// player1.coordinates[currentRound]
+		// enemy has to check all possible directions and then make a decision on which direction to go
+		
+		/*
+		if(enemy.direction == 'up') {
+			posy = enemy.coordinates[currentRound].posy - 1, // Make these the if check not direction
+			posx = enemy.coordinates[currentRound].posx
+		}else if(enemy.direction == 'down') {
+			posy = enemy.coordinates[currentRound].posy + 1,
+			posx = enemy.coordinates[currentRound].posx
+		}else if(enemy.direction == 'left') {
+			posy = enemy.coordinates[currentRound].posy,
+			posx = enemy.coordinates[currentRound].posx - 1
+		}else if(enemy.direction == 'right') {
+			posy = enemy.coordinates[currentRound].posy,
+			posx = enemy.coordinates[currentRound].posx + 1
+		}
+
+		var player1Length = player1.coordinates.Length;
+		for(var i = 0; i < player1Length; i++) {
+			// if current direction is good stay course
+			// option 1 go up
+			// option 2 go down
+			// option 3 go left
+			// option 4 go right
+		}
+		*/
+
+		switch(enemy.direction) {
+			case 'up':
+				posy = enemy.coordinates[currentRound].posy - 1;
+				posx = enemy.coordinates[currentRound].posx;
+				break;
+			case 'down':
+				posy = enemy.coordinates[currentRound].posy + 1;
+				posx = enemy.coordinates[currentRound].posx;
+				break;
+			case 'left':
+				posy = enemy.coordinates[currentRound].posy;
+				posx = enemy.coordinates[currentRound].posx - 1;
+				break;
+			case 'right':
+				posy = enemy.coordinates[currentRound].posy;
+				posx = enemy.coordinates[currentRound].posx + 1;
+				break;
+			default:
+				posy = enemy.coordinates[currentRound].posy;
+				posx = enemy.coordinates[currentRound].posx;
+				break;
+		}
+
+		// if(enemy.direction == 'up') {
+		// 	posy = enemy.coordinates[currentRound].posy - 1,
+		// 	posx = enemy.coordinates[currentRound].posx
+		// }else if(enemy.direction == 'down') {
+		// 	posy = enemy.coordinates[currentRound].posy + 1,
+		// 	posx = enemy.coordinates[currentRound].posx
+		// }else if(enemy.direction == 'left') {
+		// 	posy = enemy.coordinates[currentRound].posy,
+		// 	posx = enemy.coordinates[currentRound].posx - 1
+		// }else if(enemy.direction == 'right') {
+		// 	posy = enemy.coordinates[currentRound].posy,
+		// 	posx = enemy.coordinates[currentRound].posx + 1
+		// }
+
+		// var coords = updateCoordinates(enemy);
+		// console.log(coords);
+
+		enemy.coordinates[nextRound] = { 
+			posy: posy,
+			posx: posx
+		}
+	}
+
+	return playerAI;
+
+});
